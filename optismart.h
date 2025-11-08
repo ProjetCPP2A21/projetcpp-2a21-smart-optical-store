a95c4173408d4a93173f28aeddc7e4ef80108831
Adem <adem.belkhir@esprit.tn>
2025-11-08 12:44:32 +0100

    projet integré adem et nour  .

--- optismart.h
+++ optismart.h
@@ -8,6 +8,7 @@
 #include<QSqlQueryModel>
 #include <QWidget>
 #include "ui_optismart.h"
+#include "produit.h"
 
 
 
@@ -25,10 +26,29 @@ class optismart : public QMainWindow
 public:
     optismart(QWidget *parent = nullptr);
     ~optismart();
-
-
+    //client
+private slots:
+    void on_bajouter_c_clicked();
+private slots:
+    void on_bsupprimer_c_clicked();
+    /*void afficher_client();*/
+    void afficher_client(int id_selectionne = -1);
+private slots:
+    void on_tableWidget_c_cellClicked(int row, int column);
+    void on_bmodifier_c_clicked();
+    // produit
+    void on_ajouterButton_clicked();
+    void on_supprimerButton_clicked();
+    void on_modifierButton_clicked();
+    void on_tableWidget_p_2_doubleClicked(const QModelIndex &index);
+    void on_searchButton_clicked();
+    void on_exportPdfButton_clicked();
+    void on_statsButton_clicked();
+    void on_trierButton_clicked();
 private:
     Ui::optismart *ui;
+    produit Ptmp;
+    int selectedIdProduit;
 };
 
 #endif // OPTISMART_H
